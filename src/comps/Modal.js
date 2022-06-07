4dc8129770d3ac737797e6871314a2e72ff74f18
mgsrevolver <clayreimus@gmail.com>
2022-06-07 12:33:28 -0400

    cosmetic updates

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from 'react'
+import { motion } from 'framer-motion'
 
 const Modal = ({ setSelectedImg, selectedImg }) => {
-
   const handleClick = (e) => {
     if (e.target.classList.contains('backdrop')) {
-      setSelectedImg(null);
+      setSelectedImg(null)
     }
   }
 
   return (
-    <motion.div className="backdrop" onClick={handleClick}
+    <motion.div
+      className="backdrop"
+      onClick={handleClick}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
-      <motion.img src={selectedImg} alt="enlarged pic" 
-        initial={{ y: "-100vh" }}
+      <motion.img
+        src={selectedImg}
+        alt="enlarged pic"
+        initial={{ y: '-100vh' }}
         animate={{ y: 0 }}
       />
     </motion.div>
   )
 }
 
-export default Modal;
+export default Modal
