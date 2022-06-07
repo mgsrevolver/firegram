ee4c18f92ea477e4fda2c30bf3f65c8191d72703
mgsrevolver <clayreimus@gmail.com>
2022-06-07 09:48:53 -0400

    switched to shortygram

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>ShortyGram</h1>
     </div>
   )
 }
 
-export default Title;
+export default Title
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,22 +1,22 @@
-import * as firebase from 'firebase/app';
-import 'firebase/storage';
-import 'firebase/firestore';
+import * as firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDLmnoBvnjOQ-AaJgDw1DnVBiTETbcp8LI",
-  authDomain: "the-net-ninja-sandbox.firebaseapp.com",
-  databaseURL: "https://the-net-ninja-sandbox.firebaseio.com",
-  projectId: "the-net-ninja-sandbox",
-  storageBucket: "the-net-ninja-sandbox.appspot.com",
-  messagingSenderId: "485942827092",
-  appId: "1:485942827092:web:1811d9d8f1f5fabcd5b5c1"
-};
+  apiKey: 'AIzaSyAx5GqBSUdxKuNpAoDnJ1ebMjeam-PO4UI',
+  authDomain: 'shorty-gallery.firebaseapp.com',
+  // databaseURL: 'https://the-net-ninja-sandbox.firebaseio.com',
+  projectId: 'shorty-gallery',
+  storageBucket: 'shorty-gallery.appspot.com',
+  messagingSenderId: '614086498790',
+  appId: '1:614086498790:web:829f34b61ec8932df1de27',
+}
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
-const projectStorage = firebase.storage();
-const projectFirestore = firebase.firestore();
-const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+const projectStorage = firebase.storage()
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export { projectStorage, projectFirestore, timestamp };
+export { projectStorage, projectFirestore, timestamp }
